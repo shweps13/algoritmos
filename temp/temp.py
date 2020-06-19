24bdeaf6f3ccfc1aa912ad62f09507732f8d6104
Heorhii Siburov <g.siburov@gmail.com>
2020-06-18 17:59:30 -0700

    print temp

--- temp/temp.py
+++ temp/temp.py
@@ -2,10 +2,11 @@ class Solution(object):
     def dailyTemperatures(self, T):
         nxt = [float('inf')] * 102
         ans = [0] * len(T)
-        for i in xrange(len(T) - 1, -1, -1):
+        for i in range(len(T) - 1, -1, -1):
             #Use 102 so min(nxt[t]) has a default value
-            warmer_index = min(nxt[t] for t in xrange(T[i]+1, 102))
+            warmer_index = min(nxt[t] for t in range(T[i]+1, 102))
             if warmer_index < float('inf'):
                 ans[i] = warmer_index - i
             nxt[T[i]] = i
+        print(ans)
         return ans
